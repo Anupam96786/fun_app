1f4121d0fb21cfa8a3d1d996265dcf3e4e104ee8
Anupam96786 <anupam96786@gmail.com>
2020-08-04 05:59:39 +0530

    signup (edit mode)

--- fun_app/urls.py
+++ fun_app/urls.py
@@ -20,4 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
--- fun_app/views.py
+++ fun_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        user = User()
+    return render(request, 'signup.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'login.html')
