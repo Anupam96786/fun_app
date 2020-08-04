d754e6156d9b4614a99d0fd7f84be2da4041e835
Anupam96786 <anupam96786@gmail.com>
2020-08-04 06:46:19 +0530

    design

--- fun_app/views.py
+++ fun_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 
 def home(request):
@@ -8,11 +9,30 @@ def home(request):
 
 def signup(request):
     if request.method == 'POST':
-        user = User()
-    return render(request, 'signup.html')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = User(username=username, password=password)
+        user.save()
+        del user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
 
 
 def login(request):
     if request.method == 'POST':
-        pass
-    return render(request, 'login.html')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
